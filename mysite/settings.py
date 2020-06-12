e7fe05410a76adb109f53d51c6644a2acdfdec9d
unknown <ixh840@student.bham.ac.uk>
2020-06-12 20:31:32 +0100

    Merge fixes

--- mysite/settings.py
+++ mysite/settings.py
@@ -121,6 +121,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+#storage file for user uploaded images and documents
+
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media') 
 MEDIA_URL = '/media/'
 
