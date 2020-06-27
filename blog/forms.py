9fca33f6e68fd696678e48bdde53ecc9ad30c507
unknown <ixh840@student.bham.ac.uk>
2020-06-27 16:24:29 +0100

    Blog styling update

--- blog/forms.py
+++ blog/forms.py
@@ -7,3 +7,8 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('title','coverImage', 'text')
+        widgets = {
+        	'title' : forms.TextInput(attrs={'placeholder': 'Enter Post title'}),
+        	'text' : forms.Textarea(attrs={'placeholder' : 'Type your blog here...', 'oninput' : 'this.style.height = "";this.style.height = this.scrollHeight + "px"'})
+        	
+        }
--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+from django import forms
 from django.utils import timezone
 import os
 
